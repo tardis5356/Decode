38e575ad4293aa64c75e7e2c846a929fd9471671
victor <ftctardis@gmail.com>
2025-09-07 19:38:48 -0400

    9/7/25 Start of teleop with a little bit of logic for the Spindex and establishing the Slot class for said logic. -Victor Roussev

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/GlobalVariables.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+public class GlobalVariables {
+
+    //These are variables used throughout the code that may change.
+
+    public static String motif = new String();
+
+
+}
