03cc05c23f5f5aa4a9e5acc2c7ca51e07e35cc9d
Eavan <eavanqin0716@gmail.com>
2025-10-26 15:07:14 -0400

    tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeBot/Auto/PinpointLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeBot/Auto/PinpointLocalizer.java
@@ -16,8 +16,8 @@ import java.util.Objects;
 @Config
 public final class PinpointLocalizer implements Localizer {
     public static class Params {
-        public double parYTicks = 0.0; // y position of the parallel encoder (in tick units)
-        public double perpXTicks = 0.0; // x position of the perpendicular encoder (in tick units)
+        public double parYTicks = 3946.3673650245687; // y position of the parallel encoder (in tick units)
+        public double perpXTicks = -189.69082545930533; // x position of the perpendicular encoder (in tick units)
     }
 
     public static Params PARAMS = new Params();
