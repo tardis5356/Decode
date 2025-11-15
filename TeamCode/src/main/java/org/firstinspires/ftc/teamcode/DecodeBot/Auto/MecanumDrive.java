06e4ee3694f983dd900c35c0d6e52a2d155b03ef
trant <ftctardis@gmail.com>
2025-11-15 17:44:40 -0500

    relocalization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeBot/Auto/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeBot/Auto/MecanumDrive.java
@@ -82,9 +82,9 @@ public final class MecanumDrive {
         public double kA = 0.00003;//0.00002
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 50;
-        public double minProfileAccel = -30;
-        public double maxProfileAccel = 50;
+        public double maxWheelVel = 50; //50
+        public double minProfileAccel = -30; //-120
+        public double maxProfileAccel = 50; //100
 
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
