0acc66282fb6975003cec5075864cd58ff67b001
trant <ftctardis@gmail.com>
2025-12-08 07:30:14 -0500

    12/7/2025

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zenith/Auto/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zenith/Auto/MecanumDrive.java
@@ -63,6 +63,7 @@ import java.util.List;
 @Config
 public final class MecanumDrive {
     //Boo! Victor Jumpscare!!!
+    //😲
     public static class Params {
         // IMU orientation
         // TODO: fill in these values based on
