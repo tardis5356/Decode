70fed2babd3f731ce6591fe750112c8b39dd0f7d
trant <ftctardis@gmail.com>
2026-01-04 12:38:18 -0500

    new Shooting data added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zenith/Subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Zenith/Subsystems/Shooter.java
@@ -25,7 +25,7 @@ public class Shooter extends SubsystemBase {
     //idk if vS is necessary but that's just there so the motor is at a power always at the brink of surpassing the force of static friction.
     public static float vP = 0.0095f, vI = 0, vD = 0.0005f, vV = 0.007f, vS = 0;
 
-    public static double hardWheelOffset = 10;
+    public static double hardWheelOffset = 0;
 
     public static double neededVoltage, dutyCycle, batteryVoltage;
 
@@ -77,35 +77,63 @@ public class Shooter extends SubsystemBase {
 
         sH.setPosition(0.05);
 
-
+// OLD SHOOTING DATA
         //prep regression data
         //small triangle
-        HoodRegression.put(152., .05);
-        HoodRegression.put(126.7, .078);
-        HoodRegression.put(96.6, .05);
-        HoodRegression.put(75.7, .071);
-        HoodRegression.put(54.5,.1);
-        HoodRegression.put(34.5,.1);
-        HoodRegression.put(18.,.85);
+//        HoodRegression.put(152., .05);
+//        HoodRegression.put(126.7, .078);
+//        HoodRegression.put(96.6, .05);
+//        HoodRegression.put(75.7, .071);
+//        HoodRegression.put(54.5,.1);
+//        HoodRegression.put(34.5,.1);
+//        HoodRegression.put(18.,.85);
+//
+//        //MDRegression.put(115., .05);
+//        WheelRegression.put(152., 1450.-hardWheelOffset);
+//        WheelRegression.put(149.,1450.-hardWheelOffset);
+//        //WheelRegression.put(126.7, 1325.-hardWheelOffset);
+//        //TODO: CHECK THESE POINTS
+//        WheelRegression.put(126.7, 1365.-hardWheelOffset);
+//        WheelRegression.put(120.,1365-hardWheelOffset);
+//        WheelRegression.put(110.,1100-hardWheelOffset);
+//
+//        //TODO:Check this point
+//        WheelRegression.put(120.,1350-hardWheelOffset);
+//        WheelRegression.put(110.,1100-hardWheelOffset);
+//
+//        WheelRegression.put(96.6,1100.-hardWheelOffset);
+//        WheelRegression.put(75.7,1075.-hardWheelOffset);
+//        WheelRegression.put(54.5,975.-hardWheelOffset);
+//        WheelRegression.put(34.5,975.-hardWheelOffset);
+//        WheelRegression.put(18.,850.-hardWheelOffset);
 
-        //MDRegression.put(115., .05);
-        WheelRegression.put(152., 1450.-hardWheelOffset);
-        WheelRegression.put(149.,1450.-hardWheelOffset);
-        //WheelRegression.put(126.7, 1325.-hardWheelOffset);
-        //TODO: CHECK THESE POINTS
-        WheelRegression.put(126.7, 1365.-hardWheelOffset);
-        WheelRegression.put(120.,1365-hardWheelOffset);
-        WheelRegression.put(110.,1100-hardWheelOffset);
 
-        //TODO:Check this point
-        WheelRegression.put(120.,1350-hardWheelOffset);
-        WheelRegression.put(110.,1100-hardWheelOffset);
 
-        WheelRegression.put(96.6,1100.-hardWheelOffset);
-        WheelRegression.put(75.7,1075.-hardWheelOffset);
-        WheelRegression.put(54.5,975.-hardWheelOffset);
-        WheelRegression.put(34.5,975.-hardWheelOffset);
-        WheelRegression.put(18.,850.-hardWheelOffset);
+
+       ////NEW SHOOTING DATA 1/4/26
+        HoodRegression.put(139.8, 0.1);
+        HoodRegression.put(118.6, 0.1);
+        HoodRegression.put(109.0, 0.15);
+        HoodRegression.put(100.4, 0.2);
+        HoodRegression.put(90.0, 0.2);
+        HoodRegression.put(79.7, 0.2);
+        HoodRegression.put(62.0, 0.25);
+        HoodRegression.put(52.5, 0.4);
+        HoodRegression.put(42.5, 0.525);
+        HoodRegression.put(32.8, 0.625);
+
+
+        WheelRegression.put(139.8, 1410.-hardWheelOffset);
+        WheelRegression.put(118.6, 1310.-hardWheelOffset);
+        WheelRegression.put(109.0, 1230.-hardWheelOffset);
+        WheelRegression.put(100.4, 1190.-hardWheelOffset);
+        WheelRegression.put(90.0, 1090.-hardWheelOffset);
+        WheelRegression.put(79.7, 1010.-hardWheelOffset);
+        WheelRegression.put(62.0, 990.-hardWheelOffset);
+        WheelRegression.put(52.5, 870.-hardWheelOffset);
+        WheelRegression.put(42.5, 810.-hardWheelOffset);
+        WheelRegression.put(32.8, 790.-hardWheelOffset);
+
 
 
         targeting = true;
