e65ad37524fc04ccb93978c2cce73871e8fa4222
victorroussev106-lgtm <victor.roussev106@gmail.com>
2026-01-19 13:13:20 -0500

    1/19/26 Wall-E :)

---
+++ TeamCode/src/main/java/WallE/WallETeleOp.java
@@ -0,0 +1,32 @@
+package WallE;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp (name = "Wall-E_Tele")
+public class WallETeleOp extends LinearOpMode {
+
+    DcMotor mL, mR;
+    ColorSensor sL, sR;
+
+    @Override
+    public void runOpMode(){
+
+        mL = hardwareMap.get(DcMotor.class,"mL");
+        mR = hardwareMap.get(DcMotor.class,"mR");
+//        mR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        sL = hardwareMap.get(ColorSensor.class,"sL");
+        sR = hardwareMap.get(ColorSensor.class,"sR");
+
+        waitForStart();
+
+        while(opModeIsActive())   {
+            mL.setPower(gamepad1.left_stick_y - gamepad1.right_stick_x);
+            mR.setPower(gamepad1.left_stick_y + gamepad1.right_stick_x);
+        }
+    }
+}
